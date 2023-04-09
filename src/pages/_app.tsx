56f864b12221e742d660783581ca66191b2ddf13
Pablo Hernández <17086478+Hadronomy@users.noreply.github.com>
2023-04-09 17:14:41 +0100

    refactor: move font specification to `_app.tsx`

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
 import '@styles/globals.css'
+import { Roboto } from 'next/font/google'
 import type { AppProps } from 'next/app'
 
+const roboto = Roboto({ subsets: ['latin'], weight: ['100', '300', '400', '500', '700', '900']})
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component className={roboto.className} {...pageProps} />
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,9 @@
 import Image from 'next/image'
-import { Roboto } from 'next/font/google'
 import NavBar from '@components/NavBar'
-
-const roboto = Roboto({ subsets: ['latin'], weight: ['100', '300', '400', '500', '700', '900']})
 
 export default function Home() {
   return (
-    <main className={`${roboto.className} flex flex-col min-h-screen`}>
+    <main className="flex flex-col min-h-screen">
       <NavBar />
       <div className="flex flex-grow justify-center">
         <div className="flex-grow">
