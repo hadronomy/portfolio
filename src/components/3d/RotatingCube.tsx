c9f81882ca4a8e7f641337e03d6649efb2625714
Pablo Hernández <17086478+Hadronomy@users.noreply.github.com>
2023-04-10 00:39:01 +0100

    feat: add props to `RotatingCube`

--- src/components/3d/RotatingCube.tsx
+++ src/components/3d/RotatingCube.tsx
@@ -2,9 +2,9 @@ import { useState, useRef } from "react"
 import { Canvas, useFrame } from "@react-three/fiber"
 import { motion, useTime } from "framer-motion-3d"
 
-export default function RotatingCube() {
+export default function RotatingCube(props: any) {
   return (
-    <motion.mesh animate={{ rotateX: 2 * Math.PI, transition: { duration: 3, repeat: Infinity, ease: "anticipate" }}}>
+    <motion.mesh {...props} animate={{ rotateX: 2 * Math.PI, transition: { duration: 3, repeat: Infinity, ease: "anticipate" }}}>
       <motion.boxGeometry args={[2, 2, 2]} />
       <motion.meshStandardMaterial />
     </motion.mesh>
