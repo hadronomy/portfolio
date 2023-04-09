4ec508fc37affe9ac258fcb03041de41d41b91cd
Pablo Hernández <17086478+Hadronomy@users.noreply.github.com>
2023-04-09 17:15:13 +0100

    feat: add backdrop blur to navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,8 @@
-
 import Image from 'next/image'
 
 export default function NavBar() {
   return (
-    <div className="flex justify-start items-center h-25 p-3 pt-5 pr-10">
+    <div className="flex justify-start items-center h-25 p-3 pt-5 pr-10 bg-black/10 backdrop-blur-md">
       <Image
         className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert ml-5"
         src="/next.svg"
