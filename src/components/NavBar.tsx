7ff3ab3d1b3d0f9c070ac0b29ea0779ce100df78
Pablo Hernández <17086478+Hadronomy@users.noreply.github.com>
2023-04-12 19:53:38 +0100

    refactor: use `heroicons` instead of raw svg

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import { Bars3Icon } from '@heroicons/react/24/solid';
 
 const links = [
   {
@@ -37,19 +38,7 @@ export default function NavBar() {
             aria-expanded="false"
           >
             <span className="sr-only">Open main menu</span>
-            <svg
-              className="h-6 w-6"
-              aria-hidden="true"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                clip-rule="evenodd"
-              />
-            </svg>
+            <Bars3Icon className="h-6 w-6" />
           </button>
         </div>
         <div
