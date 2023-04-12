23dcb37c2d473d6e0e19873b086fb45b5ddead00
Pablo Hernández <17086478+Hadronomy@users.noreply.github.com>
2023-04-13 00:59:05 +0100

    feat: update `NavBar` styling

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import { Bars3Icon } from '@heroicons/react/24/solid';
+import { Bars3CenterLeftIcon, MoonIcon } from '@heroicons/react/24/solid';
 import { useRouter } from 'next/router';
 
 const links = [
@@ -24,26 +24,26 @@ export default function NavBar() {
   return (
     <nav className="fixed top-0 z-20 w-full border-b-[1px] border-white/20 bg-white/40 backdrop-blur-[8px] dark:bg-black/40">
       <div className="mx-auto flex max-w-screen-xl items-center justify-between p-5 px-10">
+        <div className="flex md:order-2 md:hidden">
+          <button
+            data-collapse-toggle="navbar-menu"
+            type="button"
+            className="inline-flex h-8 w-8 items-center rounded-lg p-2 text-sm text-white hover:ring-2 hover:ring-slate-50 md:hidden"
+            aria-controls="navbar-menu"
+            aria-expanded="false"
+          >
+            <span className="sr-only">Open main menu</span>
+            <Bars3CenterLeftIcon className="h-auto w-auto" />
+          </button>
+        </div>
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
           src="/next.svg"
           alt="Next.js Logo"
           width={80}
-          height={40}
+          height={80}
           priority
         />
-        <div className="flex md:order-2 md:hidden">
-          <button
-            data-collapse-toggle="navbar-menu"
-            type="button"
-            className="inline-flex items-center rounded-lg p-2 text-sm text-white hover:ring-2 hover:ring-slate-50 md:hidden"
-            aria-controls="navbar-menu"
-            aria-expanded="false"
-          >
-            <span className="sr-only">Open main menu</span>
-            <Bars3Icon className="h-6 w-6" />
-          </button>
-        </div>
         <div
           id="navbar-menu"
           className="hidden w-full items-center justify-between md:order-1 md:flex md:w-auto"
@@ -62,6 +62,11 @@ export default function NavBar() {
               </li>
             ))}
           </ul>
+        </div>
+        <div className="items-center md:flex">
+          <button className="inline-flex h-8 w-8 items-center rounded-lg p-2 text-sm text-white hover:ring-2 hover:ring-slate-50 md:hidden">
+            <MoonIcon className="h-auto w-auto" />
+          </button>
         </div>
       </div>
     </nav>
