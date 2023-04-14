c374278ae27b20091690cd4af5682a447133748f
Pablo Hernández <17086478+Hadronomy@users.noreply.github.com>
2023-04-14 18:47:32 +0100

    fix: remove `;` inside of div

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,7 +10,7 @@ const urbanist = Urbanist({
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className={`${urbanist.className}`}>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </div>
   );
 }
