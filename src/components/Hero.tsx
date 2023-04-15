7c60689bd235b51fb94ad9d67fb3b40a4b0e6228
Pablo Hernández <17086478+Hadronomy@users.noreply.github.com>
2023-04-15 04:19:10 +0100

    refactor: restructured `HomePage`

---
+++ src/components/Hero.tsx
@@ -0,0 +1,58 @@
+import { Baloo_Chettan_2, Bellota } from 'next/font/google';
+import Typewritter from 'typewriter-effect';
+import InteractiveRoom from '@components/InteractiveRoom';
+
+const balooChettan2 = Baloo_Chettan_2({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700']
+});
+
+const bellota = Bellota({
+  subsets: ['latin'],
+  weight: ['400', '400', '700']
+});
+
+export default function Hero() {
+  return (
+    <div className="relative flex flex-grow grid-cols-12 grid-rows-6 gap-4 p-10">
+      <div className="z-10 flex h-auto w-auto place-items-center justify-center rounded-2xl p-5 text-left">
+        <div className="flex flex-col">
+          <span className="text-6xl md:text-8xl">
+            <span className={`${balooChettan2.className} `}>I&apos;m </span>
+            <span className="font-semibold">Pablo</span>
+          </span>
+          <span
+            className={`${bellota.className} text-5xl font-semibold underline decoration-sky-500 md:text-8xl`}
+          >
+            Hernandez
+          </span>
+          <span className="mt-4 text-4xl will-change-contents">
+            <Typewritter
+              options={{ loop: true }}
+              onInit={(typewriter) => {
+                typewriter
+                  .typeString('Hi 👋')
+                  .pauseFor(500)
+                  .deleteAll()
+                  .typeString(`I'm a <b class="text-emerald-500">Developer</b>`)
+                  .pauseFor(1000)
+                  .deleteChars('Developer'.length)
+                  .pauseFor(1000)
+                  .deleteAll()
+                  .typeString(
+                    '<span>I 💕 <b class="text-amber-300">EVERYTHING</b></span> related to code'
+                  )
+                  .changeDeleteSpeed(50)
+                  .deleteAll()
+                  .start();
+              }}
+            />
+          </span>
+        </div>
+      </div>
+      <div className="bottom-0 right-0 z-0 hidden h-full w-full overflow-clip rounded-2xl md:absolute md:flex">
+        <InteractiveRoom />
+      </div>
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,84 +1,23 @@
+import Hero from '@components/Hero';
 import NavBar from '@components/NavBar';
-import InteractiveRoom from '@components/InteractiveRoom';
 import Head from 'next/head';
-import { Baloo_Chettan_2, Bellota } from 'next/font/google';
-import Typewritter from 'typewriter-effect';
-
-const balooChettan2 = Baloo_Chettan_2({
-  subsets: ['latin'],
-  weight: ['400', '500', '600', '700']
-});
-
-const bellota = Bellota({
-  subsets: ['latin'],
-  weight: ['400', '400', '700']
-});
 
 export default function HomePage() {
   return (
-    <>
+    <div className="h-screen snap-y snap-mandatory flex-col overflow-y-scroll">
       <Head>
         <title>Hadronomy - Pablo Hernández</title>
       </Head>
       <NavBar />
-      <main>
-        <section className="flex min-h-screen flex-col">
-          <div className="relative flex-grow grid-cols-12 grid-rows-6 gap-4 p-10">
-            <div className="h-auto w-auto rounded-2xl p-5 text-left md:absolute md:z-10 md:ml-10">
-              <div className="flex flex-col">
-                <span className="text-6xl md:text-8xl">
-                  <span className={`${balooChettan2.className} `}>
-                    I&apos;m{' '}
-                  </span>
-                  <span className="font-semibold">Pablo</span>
-                </span>
-                <span
-                  className={`${bellota.className} text-5xl font-semibold underline decoration-sky-500 md:text-8xl`}
-                >
-                  Hernandez
-                </span>
-                <span className="mt-4 text-4xl will-change-contents">
-                  <Typewritter
-                    options={{ loop: true }}
-                    onInit={(typewriter) => {
-                      typewriter
-                        .typeString('Hi 👋')
-                        .pauseFor(500)
-                        .deleteAll()
-                        .typeString(
-                          `I'm a <b class="text-emerald-500">Developer</b>`
-                        )
-                        .pauseFor(1000)
-                        .deleteChars('Developer'.length)
-                        .pauseFor(1000)
-                        .deleteAll()
-                        .typeString(
-                          '<span>I 💕 <b class="text-amber-300">EVERYTHING</b></span> related to code'
-                        )
-                        .changeDeleteSpeed(50)
-                        .deleteAll()
-                        .start();
-                    }}
-                  />
-                </span>
-              </div>
-              {/* Socials */}
-            </div>
-            <div className="bottom-0 right-0 hidden h-full w-full overflow-clip rounded-2xl md:absolute md:flex">
-              <InteractiveRoom />
-            </div>
-          </div>
-        </section>
-        <section
-          id="about"
-          className="h-[900px] snap-start border-t-2"
-        ></section>
-        <section
-          id="contact"
-          className="h-[900px] snap-start border-t-2"
-        ></section>
-      </main>
+      <section className="flex h-screen snap-start flex-col">
+        <Hero />
+      </section>
+      <section id="about" className="h-screen snap-center border-t-2"></section>
+      <section
+        id="contact"
+        className="h-screen snap-center border-t-2"
+      ></section>
       {/* Footer */}
-    </>
+    </div>
   );
 }
