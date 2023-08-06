f6c6b856fa10046524048d64696a7e634ef5060a
Pablo Hernández <17086478+Hadronomy@users.noreply.github.com>
2023-08-06 09:59:25 +0100

    chore: add `prettier-plugin-packagejson` to prettier config

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import("prettier").Config} */
 const config = {
-  plugins: [require.resolve('prettier-plugin-tailwindcss')],
+  plugins: [require.resolve('prettier-plugin-tailwindcss'), require.resolve('prettier-plugin-packagejson')],
   trailingComma: 'none',
   arrowParens: 'always',
   jsxSingleQuote: false,
