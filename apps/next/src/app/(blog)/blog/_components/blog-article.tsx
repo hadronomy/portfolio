a6baececdb1e87de8e926f85826effda29014204
Pablo Hernández <17086478+Hadronomy@users.noreply.github.com>
2023-10-14 14:18:06 +0100

    fix: adjust blog post html semantics

--- apps/next/src/app/(blog)/blog/_components/blog-article.tsx
+++ apps/next/src/app/(blog)/blog/_components/blog-article.tsx
@@ -43,11 +43,11 @@ export function BlogArticle({ post }: BlogArticleProps) {
       </header>
       <MDX code={body.code} />
       <footer className="my-8 flex h-[75vh] ring-4 ring-muted rounded-md place-items-center justify-center">
-        <h1 className="scroll-m-20 text-lg text-muted font-extrabold text-center leading-tight tracking-tight md:leading-loose lg:text-xl">
+        <small className="scroll-m-20 text-lg text-muted font-extrabold text-center leading-tight tracking-tight md:leading-loose lg:text-xl">
           <Balancer>
             This margin is intentional for mantaining eye level when reading
           </Balancer>
-        </h1>
+        </small>
       </footer>
     </article>
   );
