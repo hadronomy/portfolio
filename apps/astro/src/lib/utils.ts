630de0dd8a680e888568fdb00c64c590c93b92fd
Pablo Hernández <17086478+Hadronomy@users.noreply.github.com>
2024-10-09 19:25:34 +0100

    feat: import images dynamically

--- apps/astro/src/lib/utils.ts
+++ apps/astro/src/lib/utils.ts
@@ -1,3 +1,21 @@
+import path from 'path';
+import type { ImageMetadata } from 'astro';
+
 export async function delay(ms: number) {
   return new Promise((res) => setTimeout(res, ms));
+}
+
+const images = import.meta.glob<{ default: ImageMetadata }>(
+  '/src/assets/images/*.{jpeg,jpg,png,gif}',
+);
+
+export async function getImage(name: string) {
+  const imagePath = path.join('/src/assets/images', name);
+  const image = images[imagePath];
+  if (!image) {
+    throw new Error(
+      `${imagePath} does not exist in glob: /src/assets/*.{jpeg,jpg,png,gif}`,
+    );
+  }
+  return image();
 }
