e53227cc989e62388a4193dd9bf1cf4db4de9a8a
Pablo Hernández <17086478+hadronomy@users.noreply.github.com>
2025-02-01 22:52:41 +0000

    chore: fix formatting and linting issues

--- apps/astro/src/pages/rss.xml.ts
+++ apps/astro/src/pages/rss.xml.ts
@@ -1,6 +1,6 @@
+import { getCollection } from 'astro:content';
 import rss from '@astrojs/rss';
 import type { APIContext } from 'astro';
-import { getCollection } from 'astro:content';
 
 import { SITE_DESCRIPTION, SITE_TITLE, SITE_URL } from '~/consts';
 
